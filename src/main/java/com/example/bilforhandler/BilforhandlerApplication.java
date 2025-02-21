e385c5e0e142e8cae8574b00abb65d020d3cf55b
Zahaa Mac <Zahaawii@gmail.com>
2025-02-21 15:26:38 +0100

    initialize project

---
+++ src/main/java/com/example/bilforhandler/BilforhandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilforhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilforhandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilforhandlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bilforhandler/BilforhandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilforhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilforhandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
